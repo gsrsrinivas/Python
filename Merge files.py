1a3b56c36cc6b58e398f0d264e22d9a0acb6d2b6
gsrsrinivas+github1 <gsrsrinivas+github1@gmail.com>
2025-05-01 13:02:12 +0530

    upoad the mp3 file merging code

---
+++ Merge files.py
@@ -0,0 +1,39 @@
+from pydub import AudioSegment
+import glob
+import os
+
+folder_path = r"C:\Users\gsrsr\Downloads"
+mp3_files = []
+# Get all .mp3 files in the folder
+# files = glob.glob(folder_path + "/*.mp3")
+# mp3_files = [os.path.basename(file) for file in files]
+files = os.listdir(folder_path)
+mp3_files.extend(files)
+# for file in files:
+#     print(file)
+# print(mp3_files)
+# List of MP3 files to merge
+# mp3_files = ["file1.mp3", "file2.mp3", "file3.mp3"]
+# Load the first file
+# merged_audio = AudioSegment.from_mp3(mp3_files[0])
+i = 0
+for j, file in enumerate(files):
+    file_extension = os.path.splitext(file)[1]
+    if file_extension == ".mp3":
+        filepath = folder_path + "\\" + file
+        print(filepath)
+        audio = AudioSegment.from_mp3(folder_path+"\\"+file)
+        i = i + 1
+        if i == 1:
+            merged_audio = audio
+        else:
+            merged_audio += audio
+# Append remaining files
+# for mp3 in mp3_files[1:]:
+#     audio = AudioSegment.from_mp3(mp3)
+#     merged_audio += audio
+
+# Export the merged audio file
+merged_audio.export("merged_output.mp3", format="mp3")
+
+print("MP3 files merged successfully!")
---
+++ MergeFilesffmeg.py
@@ -0,0 +1,20 @@
+import os
+import ffmpeg
+from ffmpeg import run, input, output
+
+folder_path = "C:/Users/gsrsr/Downloads"
+output_file = "merged_output.mp3"
+
+# Get all MP3 files
+mp3_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith(".mp3")]
+
+# Create input file list
+with open("input.txt", "w") as f:
+    for file in mp3_files:
+        # f.write(f"file '{file}'\n")
+        f.write(f"{file}\n")
+
+# Run ffmpeg command to merge files
+ffmpeg.input("input.txt", format="concat", safe=0).output(output_file, codec="copy").run()
+
+print("MP3 files merged successfully!")

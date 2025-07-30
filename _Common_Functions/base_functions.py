c0a5b4867b4ea452443cee09dd837f0be7ebfa1f
gsrsrinivas+github1 <gsrsrinivas+github1@gmail.com>
2025-07-30 09:20:09 +0530

    Refactor download functions for improved clarity and remove deprecated code

--- _Common_Functions/base_functions.py
+++ _Common_Functions/base_functions.py
@@ -237,48 +237,6 @@ def insert_new_columns_in_data_frame(df, tf_l_i, each_segment_list, batch_no):
     return df
 
 
-def download_chart_ink_technical_analysis_scanner_bkp(data_each_list, each_segment_list, batch_no, max_retries = 5):
-    """
-    Fetches technical analysis data from Chart ink based on provided scan parameters.
-    Args:
-        data_each_list (dict): A dictionary containing scan parameters for Chart ink.
-    Returns:
-        pd.DataFrame: A DataFrame containing the scan results.
-    Raises:
-        KeyError: If the expected keys are not found in the response.
-    Raises:
-        requests.RequestException: If there is an issue with the HTTP request.
-        :param data_each_list:
-        :param max_retries:
-    """
-    key, data = next(iter(data_each_list.items()))
-    with requests.Session() as session:
-        response = session.get('https://chartink.com/screener/watch-list-stocks-5') # Step 1: Fetch CSRF token
-        soup = Bs(response.content, 'lxml')
-        csrf_token = soup.select_one('[name=csrf-token]')['content']
-        session.headers['X-CSRF-TOKEN'] = csrf_token
-        # session.headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8'
-        for attempt in range(max_retries): # Retry logic loop
-            try: # Step 2: Submit scan data
-                response = session.post('https://chartink.com/screener/process', data=data)
-                if response.status_code == 429:
-                    retry_after = int(response.headers.get("Retry-After", 2 ** attempt))
-                    print(f"❌ Rate limited on attempt {attempt + 1}. Retrying after {retry_after} seconds...")
-                    time.sleep(retry_after)
-                    continue
-                response.raise_for_status()
-                result = response.json()
-                if "scan_error" in result: # Step 3: Check for errors
-                    print(f"❌ Scan error:{result['scan_error']} for rule: {key} with data: {data}")
-                df = pd.DataFrame(result.get('data', [])) # Step 4: Convert to DataFrame
-                df = insert_new_columns_in_data_frame(df, key, each_segment_list, batch_no)
-                return df
-            except requests.RequestException as e:
-                print(f"❌ {data_each_list} Request failed on attempt {attempt + 1}: {e}")
-                time.sleep(2 ** attempt)  # Exponential backoff
-        return None
-
-
 def download_chart_ink_technical_analysis_scanner(key, data, segment_val, batch_no, max_retries = 5):
     """
     Fetches technical analysis data from Chart ink based on provided scan parameters.
@@ -366,40 +324,6 @@ def chart_ink_excel_file_download_and_insert_into_db(data_list, table_names):
             # batch_no = batch_no if 'batch_no' in locals() else ''
             # df = insert_new_columns_in_data_frame(df, key, each_segment_list, batch_no)
             df_all = pd.concat([df_all, df], ignore_index=True)
-    print(f"\n🔄 downloading data from the website is complete.")
-    insert_into_database_tables(df_all, table_names)
-
-
-def chart_ink_excel_file_download_and_insert_into_db_bkp(data_list, table_names):
-    """
-    Downloads technical analysis data from Chart ink for multiple segments and inserts it into a database.
-    :param data_list: It contains the scan parameters for Chart ink.
-    :param table_names: It contains the names of the database tables.
-    :return:
-    1. Download technical analysis data from Chart ink for multiple segments.
-    2. Insert the downloaded data into a database table.
-    3. Prints progress messages to the console.
-    :raises KeyError: If the expected keys are not found in the response.
-    :raises requests.RequestException: If there is an issue with the HTTP request.
-    """
-    segments = {'Cash': 'cash',
-                # 'Nifty 500':'57960','BankNifty':'136699','ETFs':'166311','Futures':'33489','Gold ETFs':'167068','Indices':'45603','Mid-Cap 50':'136492','Nifty 100':'33619','Nifty 200':'46553','Nifty 50':'33492','Nifty 500 Multi Cap 50:25:25':'1090574','Nifty and BankNifty':'109630','Nifty Large Mid-Cap 250':'1090573','Nifty Micro Cap 250':'1090582','Nifty Mid-Cap 100':'1090585','Nifty Mid-Cap 150':'1090588','Nifty Mid-Cap 50':'1090591','Nifty Mid-Cap Select':'1090579','Nifty Mid-Small Cap 400':'1090575','Nifty Next 50':'1116352','Nifty Small Cap 100':'1090587','Nifty Small Cap 250':'1090572','Nifty Small Cap 50':'1090568','Silver ETFs':'1195362',
-                }
-    df_all = pd.DataFrame()
-    batch_no = datetime.now().strftime('%Y%m%d%H%M%S')
-    for data_each_list in data_list:
-        # start - iterate through the segments for one single url
-        old_str = 'segments_filter'
-        for each_segment_list in segments:
-            new_str = segments[each_segment_list]
-            data_each_list = {key: {k: val.replace(old_str, new_str)} for key, value in data_each_list.items() for k, val in value.items()}
-            old_str = new_str
-            key = next(iter(data_each_list))  # Gets the first key
-            # end - iterate through the segments for one single url
-            df = download_chart_ink_technical_analysis_scanner(data_each_list)
-            df = insert_new_columns_in_data_frame(df, key, each_segment_list,batch_no)
-            df_all = pd.concat([df_all, df], ignore_index=True)
-            print(f"complete - '{str(key.replace("__", ";").replace("_", " ")).ljust(56,' ')}' - {each_segment_list} segments ")
     print(f"\n🔄 downloading data from the website is complete.")
     insert_into_database_tables(df_all, table_names)
 
